d1c5b5e2605f2167b2b92bba5457f2fb781f44e5
Mariusz Nowak <medyk@medikoo.com>
2017-01-12 15:13:16 +0100

    Basic input element functions

---
+++ input/is.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var isElement = require('dom-ext/html-element/is-html-element');
+
+module.exports = function (element) {
+	return Boolean(isElement(element) && (element.nodeName.toLowerCase() === 'input'));
+};
